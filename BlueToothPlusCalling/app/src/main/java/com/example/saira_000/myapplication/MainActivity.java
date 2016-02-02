234c39d92363239c99c6e63b152540a4de062d16
StefanKoeck <koecki91@web.de>
2016-02-02 12:24:46 +0100

    alles funzt, kommentare hinzugefügt

--- BlueToothPlusCalling/app/src/main/java/com/example/saira_000/myapplication/MainActivity.java
+++ BlueToothPlusCalling/app/src/main/java/com/example/saira_000/myapplication/MainActivity.java
@@ -18,6 +18,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 import java.io.BufferedReader;
@@ -29,15 +30,13 @@ import java.io.OutputStreamWriter;
 import java.util.Set;
 
 public class MainActivity extends Activity {
-    Button b1, b2;
+    Button b2;
     static boolean beingCalled = false;
 
     String stefanDilger = "0121416242646";
 
     public static OutputStream outputStream;
     public static InputStream inStream;
-    private BluetoothAdapter BA;
-    private Set<BluetoothDevice> pairedDevices;
 
 
     public String stateToBluetoothString(String state) {
@@ -60,7 +59,7 @@ public class MainActivity extends Activity {
         }
 
         toWriteViaBluetooth += delimiter;
-        // leiser
+        // lower
         if (state.equals("015")) {
             toWriteViaBluetooth += "1";
             validInput = true;
@@ -69,7 +68,7 @@ public class MainActivity extends Activity {
         }
 
         toWriteViaBluetooth += delimiter;
-        // lauter
+        // louder
         if (state.equals("045")) {
             toWriteViaBluetooth += "1";
             validInput = true;
@@ -94,135 +93,144 @@ public class MainActivity extends Activity {
         toWriteViaBluetooth += "\n";  // newline
 
 
+        // Sign to call stefan is detected, call him.
         if (state.equals(stefanDilger)) {
             call();
         }
-
         return toWriteViaBluetooth;
 
     }
 
+
+    // a thread, that runs in the background. You can turn off your phone, do something else, the
+    // thread is still running.
     Thread thread2 = new Thread() {
         public void run() {
             Looper.prepare();
-            Log.e("error", "Initiation.");
-            BluetoothAdapter blueAdapter = BluetoothAdapter.getDefaultAdapter();
-            if (blueAdapter != null) {
-                if (blueAdapter.isEnabled()) {
-                    Set<BluetoothDevice> bondedDevices = blueAdapter.getBondedDevices();
+            Log.e("Lets go: ", "Initiation.");
+            while (true) { // search for pairable devices for ever
+                BluetoothAdapter blueAdapter = BluetoothAdapter.getDefaultAdapter();
+                if (blueAdapter != null) {
+                    if (blueAdapter.isEnabled()) {
+                        Set<BluetoothDevice> bondedDevices = blueAdapter.getBondedDevices();
 
-                    if(bondedDevices.size() > 0){
-                        for (int i = 0; i < bondedDevices.toArray().length; i++) {
-                            try {
-                                Log.e("error", "Pairable device found.");
-                                BluetoothDevice  device = (BluetoothDevice) bondedDevices.toArray()[i];
-                                device.getBluetoothClass();
+                        if (bondedDevices.size() > 0) {
+                            for (int i = 0; i < bondedDevices.toArray().length; i++) {
+                                try {
+                                    Log.e("error", "Pairable device found.");
+                                    BluetoothDevice device = (BluetoothDevice) bondedDevices.toArray()[i];
+                                    device.getBluetoothClass();
 
-                                if (!device.getName().contains("SeeedBTSlave")) {
+                                    if (!device.getName().contains("SeeedBTSlave")) {
 
-                                    Log.e("Der name ist:", device.getName());
-                                    // continue;
+                                        Log.e("Der name ist:", device.getName());
+                                        // continue;
+                                        Toast.makeText(getApplicationContext(),
+                                                device.getName(),
+                                                Toast.LENGTH_LONG).show();
 
-                                } else {
-                                    Log.e("Richtiger Name:", device.getName());
-
-                                }
-                                Log.e("Der name ist:", device.getName());
-
-
-                                ParcelUuid[] uuids = device.getUuids();
-                                BluetoothSocket socket = device.createRfcommSocketToServiceRecord(uuids[0].getUuid());
-                                Log.e("error", "start connecting");
-
-                                socket.connect();
-                                String str = "" + socket.isConnected();
-                                Log.e("connectionResult", str);
-
-                                outputStream = socket.getOutputStream();
-                                OutputStreamWriter oReader = new OutputStreamWriter(outputStream);
-                                BufferedWriter bwriter = new BufferedWriter(oReader);
-
-                                inStream = socket.getInputStream();
-                                InputStreamReader iReader = new InputStreamReader(inStream);
-                                BufferedReader br = new BufferedReader(iReader);
-                                double ax = 0;
-                                double ay = 0;
-                                double az = 0;
-
-                                for (int j = 0; j < 1; j++) {
-                                    bwriter.write("5");
-                                    bwriter.newLine();
-                                    bwriter.flush();
-                                    String print = "" + j;
-                                    Log.e("Initializing", print);
-                                }
-
-                                while (true) {
-
-                                    String readInputLine = br.readLine();
-                                    Log.e("Der input ist:", readInputLine);
-
-                                    String[] splitInput = readInputLine.split(";");
-
-                                    String toWriteViaBluetooth = "";
-
-                                    if (splitInput[0].indexOf('0') != 0) {
-                                        Log.e("Invalid Input:", splitInput[0]);
-                                        toWriteViaBluetooth = "f";
                                     } else {
-                                        // valid input, continue giving useful output
-                                        toWriteViaBluetooth = stateToBluetoothString(splitInput[0]);
+                                        Log.e("Richtiger Name:", device.getName());
+
+                                    }
+                                    Log.e("Der name ist:", device.getName());
+
+
+                                    ParcelUuid[] uuids = device.getUuids();
+                                    BluetoothSocket socket = device.createRfcommSocketToServiceRecord(uuids[0].getUuid());
+                                    Log.e("error", "start connecting");
+
+                                    socket.connect();
+                                    String str = "" + socket.isConnected();
+                                    Log.e("connectionResult", str);
+
+                                    outputStream = socket.getOutputStream();
+                                    OutputStreamWriter oReader = new OutputStreamWriter(outputStream);
+                                    BufferedWriter bwriter = new BufferedWriter(oReader);
+
+                                    inStream = socket.getInputStream();
+                                    InputStreamReader iReader = new InputStreamReader(inStream);
+                                    BufferedReader br = new BufferedReader(iReader);
+                                    double ax = 0;
+                                    double ay = 0;
+                                    double az = 0;
+
+                                    for (int j = 0; j < 1; j++) {
+                                        bwriter.write("5");
+                                        bwriter.newLine();
+                                        bwriter.flush();
+                                        String print = "" + j;
+                                        Log.e("Initializing", print);
                                     }
 
+                                    while (true) {
 
-                                    bwriter.write(toWriteViaBluetooth);
-                                  //   bwriter.newLine();
-                                    bwriter.flush();
+                                        String readInputLine = br.readLine();
+                                        Log.e("Der input ist:", readInputLine);
+
+                                        String[] splitInput = readInputLine.split(";");
+
+                                        String toWriteViaBluetooth = "";
+
+                                        if (splitInput[0].indexOf('0') != 0) {
+                                            Log.e("Invalid Input:", splitInput[0]);
+                                            toWriteViaBluetooth = "f";
+                                        } else {
+                                            // valid input, continue giving useful output
+                                            toWriteViaBluetooth = stateToBluetoothString(splitInput[0]);
+                                        }
+
+
+                                        bwriter.write(toWriteViaBluetooth);
+                                        bwriter.flush();
+                                    }
+                                } catch (Throwable t) {
+                                    Log.e("error", "No appropriate paired devices, lets try again.");
                                 }
-                            } catch (Throwable t) {
-                                Log.e("error", "No appropriate paired devices.");
                             }
                         }
+                        Log.e("error", "No appropriate paired devices.");
+                    } else {
+                        Log.e("error", "Bluetooth is disabled.");
+                        Toast.makeText(getApplicationContext(),
+                                "Bluetooth is disabled",
+                                Toast.LENGTH_LONG).show();
                     }
-
-                    Log.e("error", "No appropriate paired devices.");
-                }else{
-                    Log.e("error", "Bluetooth is disabled.");
                 }
             }
         }
     };
 
 
+    // once the app is started, connect to bluetooth (thread2)
+    // and enable the "learn" button
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        thread2.start();
 
-
-        b1=(Button)findViewById(R.id.b1);
-
+        // call a phonestateListener to check if we are being called
         TelephonyManager TelephonyMgr = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
         TelephonyMgr.listen(new CallStateListener(this), PhoneStateListener.LISTEN_CALL_STATE);
-
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                call();
-            }
-        });
 
 
         b2=(Button)findViewById(R.id.b2);
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                thread2.start();
+                Toast.makeText(getApplicationContext(),
+                        "Not yet implemented",
+                        Toast.LENGTH_LONG).show();
             }
         });
+
     }
 
 
+    // call a predefined person
+    // additional numbers might be learned later on
+    // this calls Stefan D.
     private void call() {
         try {
             Intent callIntent = new Intent(Intent.ACTION_CALL);
